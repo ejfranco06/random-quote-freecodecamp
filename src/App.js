654355e7d541822d1477e65c986ca7f17f5b9466
Emilio J. Franco <ejfranco06@gmail.com>
2019-08-12 08:11:15 -0400

    add quote box wrapper

--- src/App.js
+++ src/App.js
@@ -5,12 +5,16 @@ import './App.css';
 class App extends Component {
   constructor(props){
     super(props)
+    this.state={
+      content: "The greatest discovery of our generation is that human beings can alter their lives by altering their attitudes of mind. As you think, so shall you be.",
+      author: "William James" 
+    }
   }
 
   render(){
     return (
       <div id='quote-box'>
-        <h1>Random Quote Generator</h1>
+        <p id='text'><i class="fas fa-quote-left"></i> {this.state.content} <i class="fas fa-quote-right"></i></p>
       </div>
     );
   }
