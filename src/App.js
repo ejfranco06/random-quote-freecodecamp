3392944baaeab6df183a7518899b63c90220fb97
Emilio J. Franco <ejfranco06@gmail.com>
2019-08-12 09:50:45 -0400

    add random quote functionality

--- src/App.js
+++ src/App.js
@@ -1,28 +1,63 @@
-import React, {Component} from 'react';
-import ReactDOM from 'react-dom';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
-  constructor(props){
-    super(props)
-    this.state={
-      content: "The greatest discovery of our generation is that human beings can alter their lives by altering their attitudes of mind. As you think, so shall you be.",
-      author: "William James" 
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      content: "",
+      author: "",
+      tweetAddr: ""
+    };
+    this.getQuote = this.getQuote.bind(this);
   }
 
-  render(){
+  async getQuote() {
+    let data = null;
+    const twitterHREF = "https://twitter.com/intent/tweet?text=";
+    try {
+      const response = await fetch("https://api.quotable.io/random");
+      data = await response.json();
+    } catch (error) {
+      console.log(error);
+    }
+    if (data != null)
+      this.setState({
+        content: data.content,
+        author: data.author,
+        tweetAddr: `${twitterHREF}"${data.content}" ${data.author}`
+      });
+  }
+
+  componentDidMount() {
+    this.getQuote();
+  }
+
+  render() {
     return (
-      <main id='quote-box'>
-        <p id='text'><i class="fas fa-quote-left"></i> {this.state.content} <i class="fas fa-quote-right"></i></p>
-        <p id='author'>-{this.state.author}</p>
-        <div>
-          <button id='new-quote'>new quote</button>
+      <main id="quote-box">
+        <p id="text">
+          <i className="fas fa-quote-left" /> {this.state.content}{" "}
+          <i className="fas fa-quote-right" />{" "}
+        </p>
+        <p id="author">-{this.state.author}</p>
+        <div className="button-row">
+          <button id="new-quote" className="button" onClick={this.getQuote}>
+            new quote
+          </button>
+          <a
+            id="tweet-quote"
+            className="button"
+            target="_blank"
+            rel="noopener noreferrer"
+            href={this.state.tweetAddr}
+          >
+            <i className="fab fa-twitter" />
+          </a>
         </div>
       </main>
     );
   }
-  
 }
 
 export default App;
